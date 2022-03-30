a8f4a442a27ab7230b75b0cea5e27ffb3519f5ec
Luis <51188376+luisalazago@users.noreply.github.com>
2022-03-30 15:06:15 -0500

    cake solved

--- Google Challenge/the_cake_is_not_a_lie.py
+++ Google Challenge/the_cake_is_not_a_lie.py
@@ -7,12 +7,4 @@ def solution(s):
             ans = s.count(temp)
             break
     return ans
-    """
-    for i in range(1, len(s) + 1):
-        temp = s[:i]
-        print(temp)
-        count = s.count(temp)
-        print(count)
-        if(count * i == len(s)): return count
-    """
 print(solution("abccbaabccba"))
