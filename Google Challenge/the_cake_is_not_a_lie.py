f2924575aa58b568897a1034186547d54052ad80
Luis <51188376+luisalazago@users.noreply.github.com>
2022-03-30 15:03:51 -0500

    Try 1

--- Google Challenge/the_cake_is_not_a_lie.py
+++ Google Challenge/the_cake_is_not_a_lie.py
@@ -1,10 +1,18 @@
 def solution(s):
-    
+    temp = ""
+    ans = None
+    for i in range(len(s)):
+        temp += s[i]
+        if(s.count(temp) * (i + 1) == len(s)):
+            ans = s.count(temp)
+            break
+    return ans
+    """
     for i in range(1, len(s) + 1):
         temp = s[:i]
         print(temp)
         count = s.count(temp)
         print(count)
         if(count * i == len(s)): return count
-
-print(solution("abcabcabcabc"))
+    """
+print(solution("abccbaabccba"))
